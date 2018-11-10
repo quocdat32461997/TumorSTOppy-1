bfe78664adf151dfef7f57fe66024a04836babc4
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 09:59:19 -0600

    touched up error_evaluation

--- TCR/measures.py
+++ TCR/measures.py
@@ -6,21 +6,26 @@ from distances import *
 def error_evaluation(weights, data, gaps=0):
     N1, N2 = map(len, data)
     M1, M2, M3 = 0, 0, 0
-    data = data.values()
 
     for i in range(N1):
         for j in range(i+1,N1):
-            M1 += blosum62_distance(tumor[i],tumor[j],weights)
+            M1 += list(blosum62_distance(data['tumorous'][i],
+                                         data['tumorous'][j],
+                                         weights, gaps))[0]
     M1 /= N1*N1
 
     for i in range(N1):
         for j in range(N2):
-            M2 += blosum62_distance(tumor[i],nontumor[j],weights)
+            M2 += list(blosum62_distance(data['tumorous'][i],
+                                         data['benign'][j],
+                                         weights, gaps))[0]
     M2 /= N1*N2
 
     for i in range(N2):
         for j in range(i+1,N2):
-            M3 += distance(nontumor[i],nontumor[j],weights)
+            M3 += list(blosum62_distance(data['benign'][i],
+                                         data['benign'][j],
+                                         weights, gaps))[0]
     M3 /= N2*N2
 
     return (M1 + M3) / M2
