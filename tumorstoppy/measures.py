825e93bf6f2a425d32cbb1ff88189ab939bb1b6f
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 11:52:34 -0600

    optimized error_evaluation

--- tumorstoppy/measures.py
+++ tumorstoppy/measures.py
@@ -6,27 +6,26 @@ from distances import *
 def error_evaluation(weights, data, gaps=0):
     N1, N2 = map(len, data)
     Mean1, Mean2, Mean3 = 0, 0, 0
+    data1, data2 = data.values()
 
     for i in range(N1):
         for j in range(i+1,N1):
-            if not j % 1000:
-                print('({}, {})'.format(i,j))
-            Mean1 += list(blosum62_distance(data['tumorous'][i],
-                                            data['tumorous'][j],
+            Mean1 += list(blosum62_distance(data1[i],
+                                            data1[j],
                                             weights, gaps))[0]
     Mean1 /= N1*N1
 
     for i in range(N1):
         for j in range(N2):
-            Mean2 += list(blosum62_distance(data['tumorous'][i],
-                                            data['benign'][j],
+            Mean2 += list(blosum62_distance(data1[i],
+                                            data2[j],
                                             weights, gaps))[0]
     Mean2 /= N1*N2
 
     for i in range(N2):
         for j in range(i+1,N2):
-            Mean3 += list(blosum62_distance(data['benign'][i],
-                                            data['benign'][j],
+            Mean3 += list(blosum62_distance(data2[i],
+                                            data2[j],
                                             weights, gaps))[0]
     Mean3 /= N2*N2
 
