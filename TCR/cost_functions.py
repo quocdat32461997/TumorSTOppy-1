0dd5e75d07ddd9470e73880fa057a0bf05481ff2
smrk007 <smk170630@utdallas.edu>
2018-11-09 20:47:13 -0600

    Added sigmoid and various other functions.

--- TCR/cost_functions.py
+++ TCR/cost_functions.py
@@ -1,3 +1,12 @@
 # Implementation of the nearest neighbor cost functions
 
+import math
 
+# Essential Functions
+def sigmoid (z):
+    return 1 / ( 1 + math.exp(-z) )
+
+def undefined_cost_function ( ):
+
+
+# should use blossom64(c1, c2)
