952e3111900761a36daaa358bc02e2439121bb67
Calvin Ross <calvinjayross@gmail.com>
2018-11-09 19:40:34 -0600

    renamed levenshtein

--- TCR/distances.py
+++ TCR/distances.py
@@ -3,14 +3,14 @@ from itertools import product
 from Bio.SubsMat import MatrixInfo
 
 def trivial_cost(c1, c2):
-    return 0 if (c1 == c2 and ' ' not in (c1,c2)) else 1
+    return 0 if (c1 == c2 and '-' not in (c1,c2)) else 1
 
 def blossum62(c1, c2):
-    return MatrixInfo.blossum62[(c1, c2)] if ' ' not in (c1, c2) else -4
+    return MatrixInfo.blossum62[(c1, c2)] if '-' not in (c1, c2) else -4
 
 def levenshtein_distance(s1, s2, cost_func=trivial_cost):
-    s1 = ' ' + s1
-    s2 = ' ' + s2
+    s1 = '-' + s1
+    s2 = '-' + s2
     l1,l2 = len(s1), len(s2)
     dist = np.zeros((l1,l2))
 
