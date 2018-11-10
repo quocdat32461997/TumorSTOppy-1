dc071d23ac70878835ce94377e037628efa84d4c
Calvin Ross <calvinjayross@gmail.com>
2018-11-10 04:10:32 -0600

    simplified error estimate

--- TCR/measures.py
+++ TCR/measures.py
@@ -6,13 +6,7 @@ from distances import *
 def error_evaluation(weights, data, gaps=0):
     N1, N2 = map(len, data)
     data = data.values()
-    M1 = list(it.accumulate(
-            blosum62_distance(data[0], data[0], weights, gaps),
-            op.add))[-1]
-    M2 = list(it.accumulate(
-            blosum62_distance(data[0], data[1], weights, gaps),
-            op.add))[-1]
-    M3 = list(it.accumulate(
-            blosum62_distance(data[1], data[1], weights, gaps),
-            op.add))[-1]
+    M1 = sum(blosum62_distance(data[0], data[0], weights, gaps))
+    M2 = sum(blosum62_distance(data[0], data[1], weights, gaps))
+    M3 = sum(blosum62_distance(data[1], data[1], weights, gaps))
     return N1*N2*(M1/N1**2+M3/N2**2)/M2
