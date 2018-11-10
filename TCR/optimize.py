ebc250658472c9b54f504b402bede65484572d33
smrk007 <smk170630@utdallas.edu>
2018-11-10 10:03:20 -0600

    Changed some names of variables for optimal readibility.

--- TCR/optimize.py
+++ TCR/optimize.py
@@ -1,11 +1,12 @@
 # Optimization functions
 import numpy as np
 from scipy.optimize import minimize
-
-# Important other functions
+import cost_functions # Will be renamed to 'measures'
 
 # TODO
 # Load the appropriate data
 # Create a partially passed-argument-function
 # Optimize based on some randomly chosen weights
 # Celebrate
+
+
